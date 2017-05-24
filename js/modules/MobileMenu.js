6f6ab7619514a6113b01111733d438c547411abe
robneal <robneal28@gmail.com>
2017-05-23 20:39:06 -0400

    finished header menu

--- js/modules/MobileMenu.js
+++ js/modules/MobileMenu.js
@@ -3,20 +3,25 @@
 
 function MobileMenu(){
 
-	this.siteHeader = $('.site-header'); 
-	this.menuIcon = $(".site-header__menu-icon");
-	this.menuContent = $('.site-header__menu-content'); 
+	this.siteHeader = $('.site-header'); // header tag
+	this.menuIcon = $(".site-header__menu-icon"); // ham menu
+	//this.menuContent = $('.site-header__menu-content'); // nothing
+	this.siteHeaderLinks = $('.site-header a'); // all links in header tag
 
 	this.events = function(){
 		this.menuIcon.click(this.toggleTheMenu.bind(this)); 
+		this.siteHeaderLinks.click(this.clearMenu.bind(this)); 
 	}
 
 	this.toggleTheMenu = function(){
-		this.menuContent.toggleClass('site-header__menu-content--is-visible');  
+		//this.menuContent.toggleClass('site-header__menu-content--is-visible');  
 		this.siteHeader.toggleClass('site-header--is-expanded'); 
 		this.menuIcon.toggleClass('site-header__menu-icon--close-x');
 	}
 
+	this.clearMenu = function(){
+		this.siteHeader.removeClass('site-header--is-expanded');
+	}
 	this.events(); 
 }
 
